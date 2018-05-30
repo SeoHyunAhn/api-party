754b270676b73ba33577eac9ebcc9be5c2ddc3d3
SeoHyun Ahn <ahn67@purdue.edu>
2018-05-29 22:17:14 -0400

    Able to get information, unable to attach it to screen

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -11,7 +11,7 @@ class RoverInfo extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            data: {},
+            rover: {},
         }
         this.fetchRoverData(this.props)
     }
@@ -32,24 +32,25 @@ class RoverInfo extends Component {
          fetch(`http://api.openweathermap.org/data/2.5/weather?q=${props.match.params.rover},${location[props.match.params.rover]}&appid=dd4b47469fab98efc9d89ea31c505dc3`)
         .then(response => response.json())
         .then(data => {
-            // const rover = {
-            //   name: data.main,
-            // //   imageUrl: data.photos[0].img_src,
-            // //   earthDate: data.photos[0].earth_date
-            // }
-            console.log(data)
-            this.setState({data})
+            const rover = {
+              name: data.main,
+              second: data.base,
+            //   imageUrl: data.photos[0].img_src,
+            //   earthDate: data.photos[0].earth_date
+            }
+            // console.log(rover)
+            this.setState({rover})
           })
             .catch((() => console.log('Well shucks')))
     }
 
     render() {
-        const { rover } = this.state.data
+        const { rover } = this.state
         console.log(rover)
         return (
             <div className="places">
                 {/* <img src={user.avatar_url} alt = ""/> */}
-                {/* <h2>Humid: {rover.main[0]}</h2> */}
+                {/* <h2>Humid: {rover.name.pop()}</h2> */}
                 {/* <h3>FOLLOWING: {user.main}</h3> */}
 
                 {/* <h3>LOCATION: {user.location}</h3>
